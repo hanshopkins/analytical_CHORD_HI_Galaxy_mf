0d77daa771d66f3ce93e3eec515dec920d0af52a
hanshopkins <hshopkin@uwaterloo.ca>
2024-03-04 18:08:19 -0500

    fully working tangent plane plot

--- tan_plane_plot.py
+++ tan_plane_plot.py
@@ -4,6 +4,9 @@ from cpp_amf_wrapper import amf
 from numpy.linalg import inv
 from math import floor, ceil
 
+def get_integer_ticks (low, high, separation):
+    return np.arange(np.ceil(low/separation)*separation, (np.floor(high/separation)+1)*separation, separation)
+
 def ang_2_3vec(phi, theta):
     return np.asarray([np.sin(theta)*np.cos(phi), np.sin(theta)*np.sin(phi), np.cos(theta)]).T
 
@@ -73,14 +76,15 @@ def tan_plane_plot (base_theta, base_phi, chord_theta, nx, ny, extent1, extent2,
         low_phi = np.min(corner_sph_coords[:,1])
         print(np.rad2deg(corner_sph_coords))
         #step 1 is to figure out if we are plotting a pole
-        pole = (np.sin(high_phi)*np.sin(low_phi)<0) and (np.cos(high_phi)*np.cos(np.sin(low_phi))<0)
+        pole = base_theta < 2 * extent2
+        #(np.sin(high_phi)*np.sin(low_phi)<0) and (np.cos(high_phi)*np.cos(low_phi)<0)
         if pole: #this super breaks it
-            e1grid = 1
+            e1grid = np.pi
             phitp = np.linspace(-e1grid, e1grid,1000) #phistoplot
-            e2grid = (high_theta+low_theta)/2
-            thetatp = np.linspace(base_theta-e2grid, base_theta+e2grid,1000) #thetastoplot
+            e2grid = high_theta
+            thetatp = np.linspace(0, e2grid,10) #thetastoplot
         else:
-            e1grid = (high_phi-low_phi)/2
+            e1grid = (high_phi-low_phi)#/2
             phitp = np.linspace(base_phi-e1grid, base_phi+e1grid,100) #phistoplot
             e2grid = (high_theta-low_theta)/2
             thetatp = np.linspace(base_theta-e2grid, base_theta+e2grid,100) #thetastoplot
@@ -90,8 +94,8 @@ def tan_plane_plot (base_theta, base_phi, chord_theta, nx, ny, extent1, extent2,
         deg_separations = np.array([1,2,5,10,30,60],dtype=int)
         phi_separation = deg_separations[np.searchsorted(deg_thresholds,np.rad2deg(e1grid))]
         theta_separation = deg_separations[np.searchsorted(deg_thresholds,np.rad2deg(e2grid))]
-        phi_ticks = np.arange(np.ceil(np.rad2deg(low_phi)/phi_separation)*phi_separation,(np.floor(np.rad2deg(high_phi)/phi_separation)+1)*phi_separation,phi_separation)
-        theta_ticks = np.arange(np.ceil(np.rad2deg(low_theta)/theta_separation)*theta_separation,(np.floor(np.rad2deg(high_theta)/theta_separation)+1)*theta_separation,theta_separation)
+        phi_ticks = get_integer_ticks(np.rad2deg(phitp[0]), np.rad2deg(phitp[-1]), phi_separation)
+        theta_ticks = get_integer_ticks(np.rad2deg(thetatp[0]), np.rad2deg(thetatp[-1]), theta_separation)
         
         #the formula for the positions of the gridlines is
         # sec(alpha)(ch_of_basis_mat @ (cos phi sin theta, sin phi sin theta, cos(theta)), where alpha is the 
@@ -99,27 +103,30 @@ def tan_plane_plot (base_theta, base_phi, chord_theta, nx, ny, extent1, extent2,
         chob = np.linalg.inv(np.hstack((v1[np.newaxis].T, v2[np.newaxis].T, basevec[np.newaxis].T)))#change of basis matrix
         for phi_deg in phi_ticks:
             phi = np.deg2rad(phi_deg)
-            secalpha = 1/(np.sin(base_phi)*np.sin(phi) + np.cos(base_phi)*np.cos(phi)*np.cos(base_theta-thetatp))
             tempvec = np.vstack((np.cos(phi)*np.sin(thetatp), np.sin(phi)*np.sin(thetatp), np.cos(thetatp)))
-            line_in_tangent_plane_coords_unscaled = secalpha * (chob @ tempvec)[:2]
+            secalpha = 1/(np.dot(tempvec.T,basevec))
+            line_in_tangent_plane_coords_unscaled = secalpha*(chob @ tempvec)[:2]
+            #assert(np.all(np.abs(np.dot((line_in_tangent_plane_coords_unscaled - np.array([[0],[0],[1]])).T, np.array([0,0,1]))) < 1e-10))
+            #line_in_tangent_plane_coords_unscaled[:2]
             #the imshow goes from -1 to 1, so we need to scale these
             line_in_tangent_plane_coords = np.empty([2,thetatp.shape[0]])
             line_in_tangent_plane_coords[0] = line_in_tangent_plane_coords_unscaled[0]/np.tan(extent2) #the top of this array should be the ys and the bottom are the xs
             line_in_tangent_plane_coords[1] = line_in_tangent_plane_coords_unscaled[1]/np.tan(extent1)
-            plt.plot(line_in_tangent_plane_coords[1], line_in_tangent_plane_coords[0],color="grey",linestyle=(0, (3, 10)))
+            plt.plot(line_in_tangent_plane_coords[1], line_in_tangent_plane_coords[0], color="grey",linestyle=(0, (3, 10)))
         for theta_deg in theta_ticks:
             theta = np.deg2rad(theta_deg)
-            secalpha = 1/(np.sin(base_phi)*np.sin(phitp) + np.cos(base_phi)*np.cos(phitp)*np.cos(base_theta-theta))
             tempvec = np.vstack((np.cos(phitp)*np.sin(theta), np.sin(phitp)*np.sin(theta), np.cos(theta)*np.ones(phitp.shape[0])))
+            secalpha = 1/(np.dot(tempvec.T,basevec))
             line_in_tangent_plane_coords_unscaled = secalpha * (chob @ tempvec)[:2]
             #the imshow goes from -1 to 1, so we need to scale these
             line_in_tangent_plane_coords = np.empty([2,phitp.shape[0]])
             line_in_tangent_plane_coords[0] = line_in_tangent_plane_coords_unscaled[0]/np.tan(extent2) #the top of this array should be the ys and the bottom are the xs
             line_in_tangent_plane_coords[1] = line_in_tangent_plane_coords_unscaled[1]/np.tan(extent1)
-            plt.plot(line_in_tangent_plane_coords[1], line_in_tangent_plane_coords[0],color="grey",linestyle=(0, (3, 10)))
+            plt.plot(line_in_tangent_plane_coords[1], line_in_tangent_plane_coords[0], color="grey",linestyle=(0, (3, 10)))
+        plt.text(-0.8,-1.1,"Gridline separations: "+str(phi_separation)+" deg (RA), "+str(theta_separation)+" deg (dec)")
     plt.show()
                 
 
 if __name__ == "__main__":
-    tan_plane_plot (np.deg2rad(45), 0, np.deg2rad(45), 300,300, np.deg2rad(3), np.deg2rad(3), 0.21, np.deg2rad(45), 0, 24, 22, 24.0*3600/1000, 1, "test tan plane plot", gridlines=True)
+    tan_plane_plot (np.deg2rad(1), 0, np.deg2rad(0), 300,300, np.deg2rad(3), np.deg2rad(3), 0.21, np.deg2rad(0), 0, 24, 22, 24.0*3600/1000, 1, "test tan plane plot", gridlines=True)
     plt.show()
